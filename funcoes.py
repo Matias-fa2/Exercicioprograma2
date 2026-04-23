145b3969f230cdf0d8997e87c5d1a21ee0096408
matias-fa2 <matiasfa@al.insper.edu.br>
2026-04-23 15:03:18 -0300

    Exercicio3

--- funcoes.py
+++ funcoes.py
@@ -16,3 +16,14 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_retirar):
     dados_rolados.append(dados_no_estoque[dado_para_retirar])
     dados_no_estoque.pop(dado_para_retirar)
     return [dados_rolados, dados_no_estoque]
+
+def calcula_pontos_regra_simples(dados_rolados):
+    pontuacao = {}
+    for dado in dados_rolados:
+        chave = str(dado)
+        pontuacao[chave] = pontuacao.get(chave, 0) + dado
+    for i in range(1,7):
+        pontuacao[str(i)] = pontuacao.get(str(i), 0)
+    return dict(sorted(pontuacao.items()))
+dados = [1,2,5,5,6]
+print(calcula_pontos_regra_simples(dados))
