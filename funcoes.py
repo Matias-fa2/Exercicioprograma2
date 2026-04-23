e2309d324fb5a137960ee16a3b0648248c15cdbf
matias-fa2 <matiasfa@al.insper.edu.br>
2026-04-23 15:22:29 -0300

    Anotacoes

--- funcoes.py
+++ funcoes.py
@@ -1,15 +1,15 @@
 import random
 
 def rolar_dados(vezes):
-    dados = []
-    for i in range(vezes):
-        dado = random.randint(1,6)
-        dados.append(dado)
+    dados = [] #cria a lista dos dados
+    for i in range(vezes): #repete o ciclo uma quantidade especifica
+        dado = random.randint(1,6) #pega um numero entre 1 e 6
+        dados.append(dado) #adiciona o numero na lista dos dados
     return dados
 
 def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
-    dados_no_estoque.append(dados_rolados[dado_para_guardar])
-    dados_rolados.pop(dado_para_guardar)
+    dados_no_estoque.append(dados_rolados[dado_para_guardar]) #adiciona o dado selecionado no estoque 
+    dados_rolados.pop(dado_para_guardar) #remove o dado selecionado dos dados rolados
     return [dados_rolados, dados_no_estoque]
 
 def remover_dado(dados_rolados, dados_no_estoque, dado_para_retirar):
@@ -18,10 +18,10 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_retirar):
     return [dados_rolados, dados_no_estoque]
 
 def calcula_pontos_regra_simples(dados_rolados):
-    pontuacao = {}
+    pontuacao = {} #cria o dicionario
     for dado in dados_rolados:
-        chave = dado
-        pontuacao[chave] = pontuacao.get(chave, 0) + dado
+        chave = dado #chave do dic = ao valor do dado
+        pontuacao[chave] = pontuacao.get(chave, 0) + dado #adiciona o valor do dado para o dicionario, se nao existir = 0
     for i in range(1,7):
-        pontuacao[i] = pontuacao.get(i, 0)
+        pontuacao[i] = pontuacao.get(i, 0) # coloca os numeros que nao sairam nos dados
     return dict(sorted(pontuacao.items()))
