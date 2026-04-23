e4d6dfda09a8042e03899a4fda99d180fa250725
matias-fa2 <matiasfa@al.insper.edu.br>
2026-04-23 12:49:26 -0300

    Correcao ex2

--- funcoes.py
+++ funcoes.py
@@ -7,7 +7,7 @@ def rolar_dados(vezes):
         dados.append(dado)
     return dados
 
-def guardar_dados(dados_rolados, dados_no_estoque, dado_para_guardar):
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     dados_no_estoque.append(dados_rolados(dado_para_guardar))
     dados_rolados.pop(dado_para_guardar)
     return [dados_rolados, dados_no_estoque]
