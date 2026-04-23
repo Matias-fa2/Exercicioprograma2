611039904dc4936b58bc32d12486ee7288fb0732
matias-fa2 <matiasfa@al.insper.edu.br>
2026-04-23 14:13:33 -0300

    Exercicio 3

--- funcoes.py
+++ funcoes.py
@@ -11,3 +11,8 @@ def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     dados_no_estoque.append(dados_rolados[dado_para_guardar])
     dados_rolados.pop(dado_para_guardar)
     return [dados_rolados, dados_no_estoque]
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_retirar):
+    dados_rolados.append(dados_no_estoque[dado_para_retirar])
+    dados_no_estoque.pop(dado_para_retirar)
+    return [dados_rolados, dados_no_estoque]
