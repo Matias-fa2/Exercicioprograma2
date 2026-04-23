36489e1f704fe26cd9627ceb10add6e034e73754
matias-fa2 <matiasfa@al.insper.edu.br>
2026-04-23 12:48:03 -0300

    Exercicio 2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,8 @@ def rolar_dados(vezes):
         dado = random.randint(1,6)
         dados.append(dado)
     return dados
+
+def guardar_dados(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_no_estoque.append(dados_rolados(dado_para_guardar))
+    dados_rolados.pop(dado_para_guardar)
+    return [dados_rolados, dados_no_estoque]
