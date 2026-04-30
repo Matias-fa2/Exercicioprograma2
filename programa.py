13a03af7324a6b1c7a66237829491fc49d46c086
matias-fa2 <matiasfa@al.insper.edu.br>
2026-04-30 14:46:15 -0300

    ex10

--- programa.py
+++ programa.py
@@ -1,9 +1,10 @@
 from funcoes import *
-
+pontuacao = 0
 rodada = 0
 dados_rolados = []
 dados_guardados = []
-
+lista_int = '123456'
+ja_foi = []
 cartela = {
     'regra_simples': {
         1: 0,
@@ -25,10 +26,47 @@ cartela = {
 
 imprime_cartela(cartela)
 while rodada < 12:
+    rerrolou = 0
     if rodada == 0:
         dados_rolados = rolar_dados(5)
-    else:
-        dados_rolados = rolar_dados(len(dados_rolados))
+    print(f'Dados rolados: {dados_rolados}')
+    print(f'Dados guardados: {dados_guardados}')
+    opcao = int(input("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:"))
+    if opcao == 1:
+        indice = int(input("Digite o índice do dado a ser guardado (0 a 4):"))
+        guardar_dado(dados_rolados, dados_guardados, indice)
+    elif opcao == 2:
+        indice = int(input("Digite o índice do dado a ser removido (0 a 4):"))
+        remover_dado(dados_rolados, dados_guardados, indice)
+    elif opcao == 3:
+        rerrolou +=1
+        if rerrolou < 3:
+            dados_rolados = rolar_dados(len(dados_rolados))
+        else:
+            print("Você já usou todas as rerrolagens.")
+        
+    elif opcao == 0:
+        combinacao = input("Digite a combinação desejada:")
+        if combinacao.isdigit(): combinacao = int(combinacao)
+        while combinacao not in cartela['regra_avancada'] and combinacao not in cartela['regra_simples']:
+            print("Combinação inválida. Tente novamente.")
+            combinacao = input("Digite a combinação desejada:")
+            if combinacao.isdigit(): combinacao = int(combinacao)
+        
+        if combinacao not in ja_foi:
+            faz_jogada(dados_rolados, combinacao, cartela)
+            ja_foi.append(combinacao)
+    elif opcao == 4:
+        imprime_cartela(cartela)
+    else: 
+        print("Opção inválida. Tente novamente.")
     
-    print()
     rodada+=1
+
+i = 0
+while i < len(cartela):
+    for combinacao in cartela[i]:
+        pontuacao += cartela[i][combinacao]
+    i+=1
+
+print(f"Pontuação total: {pontuacao}")
